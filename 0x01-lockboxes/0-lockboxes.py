38f5f5015b28b72b199de7c88f59448ffd54e011
Mythamor <bethmithamo.bm@gmail.com>
2023-12-07 11:46:44 +0300

    Using sets and list to iterate over boxes and open them

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+mudule: 0-lockboxes.py
+"""
+
+def canUnlockAll(boxes):
+    """
+    method that uses a set to keep track of unlocked boxes
+    a list to store the available keys.
+    """
+
+    # Number of boxes 
+    n = len(boxes)
+
+    # Initialize a set with the first box, that's always unlocked
+    unlocked_boxes = {0}
+
+    # Create a list of the keys in the first box
+    keys_to_try = boxes[0]
+
+    while keys_to_try:
+        key = keys_to_try.pop()
+        if key not in unlocked_boxes:
+            unlocked_boxes.add(key)
+            keys_to_try.extend(boxes[key])
+    
+    return len(unlocked_boxes) == n
