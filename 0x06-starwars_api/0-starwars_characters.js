2d4f3180ee924bd3b387cf5e637156bce0877f2c
Mythamor <bethmithamo.bm@gmail.com>
2024-01-19 01:03:11 +0300

    Asycnch rerquest to fetch data from star wars AP1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,42 +3,40 @@
 // script that prints all characters of a Star Wars movie:
 
 const request = require('request');
+
 const movieId = process.argv[2];
+const filmUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function getMovieChars (movieId) {
+// Recursive function that iterates over charUrls
+// It makes assynchronous requests for each url and logs char names to console
+function sendRequest (charList, index) {
 // Make a request to the SWAPI films endpoint
-  const filmUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+  if (index === charList.length) {
+    return;
+  }
 
-  request(filmUrl, { json: true }, (err, res, body) => {
+  request(charList[index], { json: true }, (err, res, body) => {
     if (err) {
-      console.error(`Error fetching film data ${err.message}`);
-      return;
+      console.error(`Error fetching char data ${err.message}`);
+    } else {
+      console.log(body.name);
     }
 
-    console.log(body);
-
-    const charUrls = body.characters;
-
-    // Iterate through characters url and their names
-    charUrls.forEach(charUrl => {
-      request(charUrl, { json: true }, (charErr, charRes, charBody) => {
-        if (charErr) {
-          console.error(`Error fetching char data ${charErr.message}`);
-          return;
-        }
-
-        console.log(charBody.name);
-      });
-    });
+    sendRequest(charList, index + 1);
   });
 }
-
-// Check for movieId in command line argument
 
 if (!movieId) {
   console.error('Usage: ./0-starwars_characters.js <movieId>');
   process.exit(1);
 }
 
-// Call the function to print the movie chars
-getMovieChars(movieId);
+// Asynch requests to extract list of char urls
+request(filmUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(`Error fetching char data ${err.message}`);
+  } else {
+    const charList = body.characters;
+    sendRequest(charList, 0);
+  }
+});
