230a9b43fe2392c5152fcd9636f2043b2d57a08f
Mythamor <bethmithamo.bm@gmail.com>
2024-02-13 18:48:29 +0300

    Interview question on prime numbers

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+Module: 0-prime_game
+"""
+
+
+# Helper function to find prime nums upto n
+def is_prime(n):
+    """
+    finds prime numbers upto a given value
+    """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % 1 == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+# Helper function to find winner in num of games
+def prime_numbers(n):
+    primes = []
+    for i in range(2, n + 1):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+
+def isWinner(x, nums):
+    """
+    Determines winner using prime numbers
+    """
+
+    maria = 0
+    ben = 0
+
+    for n in nums:
+        primes = prime_numbers(n)
+        if len(primes) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    if maria > ben:
+        return "Maria"
+    elif ben > maria:
+        return "Ben"
+    else:
+        return None
